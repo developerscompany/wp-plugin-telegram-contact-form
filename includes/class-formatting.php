2f0de273e8a6a689158a993ca82140f975a9569c
ffeynmann <ffeynmann@gmail.com>
2023-09-01 15:14:36 +0300

    apply notifications formatting

--- includes/class-formatting.php
+++ includes/class-formatting.php
@@ -4,15 +4,43 @@ class Rivo_WTS_Formatting
 {
     public static function get_settings_for($type)
     {
-        return Rivo_WTS_Settings_Notifications::exampleFormData();
+        return [
+            [
+                'text_before' => 'Siimme',
+                'text_after'  => 'Siimme2',
+                'bold'        => 1,
+                'italic'      => 1,
+                'icon'        => '😁'
+            ],
+            [
+                'text_before' => 'Type',
+                'text_after'  => '',
+                'bold'        => 0,
+                'italic'      => 1,
+                'icon'        => '😁'
+            ]
+        ];
 
+        return Rivo_WTS_Settings_Notifications::exampleFormData();
     }
 
     public static function apply($type, $text)
     {
-        $settings = self::get_settings_for($type);
-        //text before, after, replaces, icons etc
+        if(is_array($settings = self::get_settings_for($type))) {
+            foreach ($settings as $setting) {
+                if(empty($setting['text_before'])) {
+                    continue;
+                }
 
+                $text_replace_to = !empty($setting['text_after']) ? $setting['text_after'] : $setting['text_before'];
+
+                !empty($setting['bold']) && $text_replace_to = sprintf('<b>%s</b>', $text_replace_to);
+                !empty($setting['italic']) && $text_replace_to = sprintf('<i>%s</i>', $text_replace_to);
+                !empty($setting['icon']) && $text_replace_to = $setting['icon'] . $text_replace_to;
+
+                $text = str_replace($setting['text_before'], $text_replace_to, $text);
+            }
+        }
 
         return $text;
     }
--- includes/class-main.php
+++ includes/class-main.php
@@ -10,8 +10,5 @@ class Rivo_WTS_Main
         Rivo_WTS_Admin_Pages::init();
         Rivo_WTS_Settings::init();
         Rivo_WTS_Integrations::init();
-
-//        $token = '6418062577:AAH9GnbK6e55NZ3cznEMWNzxMpXqcmF6pVo';
-//        $data['chat_id'] = '-214862049';
     }
 }
